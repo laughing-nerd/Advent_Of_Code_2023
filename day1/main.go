9596a63b3b88a609627582f11cc2127592b97514
laughing-nerd <riturajpaul99@gmail.com>
2023-12-04 12:39:08 +0530

    Added day1, part 2

--- day1/main.go
+++ day1/main.go
@@ -5,45 +5,64 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
-func main(){
-    file, err := os.Open("./input");
-    if err != nil{
-        log.Fatal("Input file could not be opened");        
-    }
-    lines := bufio.NewScanner(file);
-    sum := 0;
+func extractDigit(v string) int {
+	switch v {
+	case "one", "1":
+		return 1
+	case "two", "2":
+		return 2
+	case "three", "3":
+		return 3
+	case "four", "4":
+		return 4
+	case "five", "5":
+		return 5
+	case "six", "6":
+		return 6
+	case "seven", "7":
+		return 7
+	case "eight", "8":
+		return 8
+	case "nine", "9":
+		return 9
+	default:
+		return 0
+	}
+}
 
-    for lines.Scan(){
-        line := lines.Text();
+func main() {
+	file, err := os.Open("./input")
+	if err != nil {
+		log.Fatal("Input file could not be opened")
+	}
+	defer file.Close()
+	lines := bufio.NewScanner(file)
+	sum := 0
+	toCheckFor := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
 
-        // 2 pointers
-        i := 0;
-        j := len(line)-1;
+	for lines.Scan() {
+		line := lines.Text()
 
-        digit_left := 0;
-        digit_right := 0;
+		//Check for the first index of a number
+		left := 0
+		right := 0
+		left_index := len(line)
+		right_index := -1
 
-        for i<=j{
-            if line[i]>48 && line[i]<58{
-                digit_left = int(line[i])-48;
-            }
-           if line[j]>48 && line[j]<58{ 
-                digit_right = int(line[j])-48;
-            }
-            if(digit_left>0 && digit_right>0){
-                break;
-            } else if digit_left>0 && digit_right == 0{
-                j -= 1;
-            } else if digit_left == 0 && digit_right > 0{
-                i += 1;
-            } else {
-                i +=1;
-                j -= 1;
-            }
-        }
-        sum = sum + (digit_left*10 + digit_right);
-    }
-    fmt.Println(sum)
+		for _, v := range toCheckFor {
+			if strings.Index(line, v) < left_index && strings.Index(line, v) >= 0 {
+				left_index = strings.Index(line, v)
+				left = extractDigit(v)
+			}
+			if strings.LastIndex(line, v) > right_index && strings.LastIndex(line, v) >= 0 {
+				right_index = strings.LastIndex(line, v)
+				right = extractDigit(v)
+			}
+		}
+        sum = sum + (left*10 + right)
+	}
+	fmt.Println(sum)
 }
